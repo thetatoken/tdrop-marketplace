31d93e0d1e185031558ac3656c7cc6d2ed1c68f8
Jieyi Long <jieyi@sliver.tv>
2021-08-31 20:33:30 +0800

    Update the test case for NFT purchase using ERC20 tokens

--- test/8-wyvern-exchanges-nft-purchases.js
+++ test/8-wyvern-exchanges-nft-purchases.js
@@ -1,149 +1,90 @@
-/* global artifacts:false, it:false, contract:false, assert:false */
-
 const WyvernAtomicizer = artifacts.require('WyvernAtomicizer')
 const WyvernExchange = artifacts.require('WyvernExchange')
-const WyvernStatic = artifacts.require('WyvernStatic')
+const StaticMarket = artifacts.require('StaticMarket')
 const WyvernRegistry = artifacts.require('WyvernRegistry')
 const TestERC20 = artifacts.require('TestERC20')
 const TestERC721 = artifacts.require('TestERC721')
-const TestERC1271 = artifacts.require('TestERC1271')
 
 const Web3 = require('web3')
 const provider = new Web3.providers.HttpProvider('http://localhost:18888')
 const web3 = new Web3(provider)
-
-const { wrap,hashOrder,ZERO_BYTES32,randomUint,NULL_SIG,assertIsRejected} = require('./aux')
+const {wrap,ZERO_BYTES32,CHAIN_ID} = require('./aux')
 
 contract('WyvernExchange-NFT-Purchase', (accounts) => {
-  const deploy = async contracts => Promise.all(contracts.map(contract => contract.deployed()))
 
-  const withContracts = async () =>
-    {
-    let [exchange,statici,registry,atomicizer,erc20,erc721,erc1271] = await deploy(
-      [WyvernExchange,WyvernStatic,WyvernRegistry,WyvernAtomicizer,TestERC20,TestERC721,TestERC1271])
-    return {exchange:wrap(exchange),statici,registry,atomicizer,erc20,erc721,erc1271}
+    let deployCoreContracts = async () => {
+        registry = await WyvernRegistry.new()
+        atomicizer = await WyvernAtomicizer.new()
+        exchange = await WyvernExchange.new(CHAIN_ID,[registry.address],'0x')
+        statici = await StaticMarket.new()
+
+        await registry.grantInitialAuthentication(exchange.address)
+        return {registry,exchange:wrap(exchange),atomicizer,statici}
     }
 
-  const withSomeTokens = async () => {
-    let {erc20, erc721} = await withContracts()
-    const amount = randomUint() + 2
-    await erc20.mint(accounts[0],amount)
-    return {tokens: amount, nfts: [1, 2, 3], erc20, erc721}
-  }
+    let deploy = async contracts => Promise.all(contracts.map(contract => contract.new()))
 
-  it('setup',async () => {
-    let {registry,erc20,erc721} = await withContracts()
+    it('purchase ERC721 NFT with ERC20 tokens', async () => {
+        let erc20MintAmount = 1000;        
+        let nftTokenID      = 7777;
+        let sellingPrice    = 99;
+        let buyingPrice     = 99;
+        let nftSeller       = accounts[6];
+        let nftBuyer        = accounts[0];
 
-    await registry.registerProxy({from: accounts[0]})
-    let proxy0 = await registry.proxies(accounts[0])
-    assert.isOk(await erc20.approve(proxy0, 100000))
-    assert.isOk(await erc721.setApprovalForAll(proxy0, true))
+        let {exchange, registry, statici} = await deployCoreContracts()
+        let [erc721] = await deploy([TestERC721])
+        let [erc20] = await deploy([TestERC20])
 
-    await registry.registerProxy({from: accounts[6]})
-    let proxy6 = await registry.proxies(accounts[6])
-    assert.isOk(await erc20.approve(proxy6, 100000, {from: accounts[6]}))
-    assert.isOk(await erc721.setApprovalForAll(proxy6, true, {from: accounts[6]}))
-  })
+        await erc721.mint(nftSeller, nftTokenID)
+        await erc20.mint(nftBuyer, erc20MintAmount)
 
-  it('purchase ERC721 NFT with ERC20 tokens',async () => {
-    let {atomicizer, exchange, registry, statici, erc20, erc721} = await withContracts()
-    let {tokens, nfts} = await withSomeTokens()
+        // -------------- Account registration and setup -------------- //
 
+        // NFT Seller
+        await registry.registerProxy({from: nftSeller})
+        let sellerProxy = await registry.proxies(nftSeller)
+        assert.equal(true, sellerProxy.length > 0, 'no proxy address for the NFT seller')
+        await erc721.setApprovalForAll(sellerProxy, true, {from: nftSeller})
 
-    // Setup account initial balances
-    let faucet    = accounts[0];
-    let nftSeller = accounts[6];
-    let nftBuyer  = accounts[0];
+        // NFT Buyer
+        await registry.registerProxy({from: nftBuyer})
+        let buyerProxy = await registry.proxies(nftBuyer)
+        assert.equal(true, buyerProxy.length > 0, 'no proxy address for the NFT buyer')
+        let maxERC20Spending = 1000;
+        await erc20.approve(buyerProxy, maxERC20Spending, {from: nftBuyer})
 
-    await erc721.transferFrom(faucet, nftSeller, nfts[0], {from: faucet})
-    assert.equal(await erc20.balanceOf(nftSeller), 0, 'Incorrect initial ERC20 balance')
-    assert.equal(await erc721.ownerOf(nfts[0]), nftSeller, 'Incorrect initial ERC721 NFT owner')
+        // -------------- Prepare for the NFT Trade -------------- //
 
-    const abi = [{'constant': false, 'inputs': [{'name': 'addrs', 'type': 'address[]'}, {'name': 'values', 'type': 'uint256[]'}, {'name': 'calldataLengths', 'type': 'uint256[]'}, {'name': 'calldatas', 'type': 'bytes'}], 'name': 'atomicize', 'outputs': [], 'payable': false, 'stateMutability': 'nonpayable', 'type': 'function'}]
-    const atomicizerc = new web3.eth.Contract(abi, atomicizer.address)
-    const erc20c = new web3.eth.Contract(erc20.abi, erc20.address)
-    const erc721c = new web3.eth.Contract(erc721.abi, erc721.address)
-    
-    // NFT Seller
-    const selectorOne = web3.eth.abi.encodeFunctionSignature('any(bytes,address[7],uint8[2],uint256[6],bytes,bytes)')
-    const extradataOne = '0x'
-    const one = {registry: registry.address, maker: nftSeller, staticTarget: statici.address, staticSelector: selectorOne, staticExtradata: extradataOne, maximumFill: '1', listingTime: '0', expirationTime: '10000000000', salt: '3352'}
-    const firstERC721Call = erc721c.methods.transferFrom(nftSeller, nftBuyer, nfts[0]).encodeABI()
-    const firstData = atomicizerc.methods.atomicize(
-      [erc721.address],
-      [0],
-      [(firstERC721Call.length - 2) / 2],
-      firstERC721Call
-    ).encodeABI()
-    const firstCall = {target: atomicizer.address, howToCall: 1, data: firstData}
-    let oneSig = await exchange.sign(one, nftSeller)
+        const erc721c = new web3.eth.Contract(erc721.abi, erc721.address)
+        const erc20c = new web3.eth.Contract(erc20.abi, erc20.address)
 
-    // NFT Buyer (purchasing with ERC20 tokens)
-    const selectorTwo = web3.eth.abi.encodeFunctionSignature('any(bytes,address[7],uint8[2],uint256[6],bytes,bytes)')
-    const extradataTwo = '0x'
-    const two = {registry: registry.address, maker: nftBuyer, staticTarget: statici.address, staticSelector: selectorTwo, staticExtradata: extradataTwo, maximumFill: '1', listingTime: '0', expirationTime: '10000000000', salt: '3335'}
-    const secondERC20Call = erc20c.methods.transferFrom(nftBuyer, nftSeller, 2).encodeABI()
-    const secondData = atomicizerc.methods.atomicize(
-      [erc20.address],
-      [0],
-      [(secondERC20Call.length - 2) / 2],
-      secondERC20Call
-    ).encodeABI()
-    const secondCall = {target: atomicizer.address, howToCall: 1, data: secondData}
-    let twoSig = await exchange.sign(two, nftBuyer)
-    
-    // Order matching
-    await exchange.atomicMatch(one, oneSig, firstCall, two, twoSig, secondCall, ZERO_BYTES32, {from: nftSeller})
-    assert.equal(await erc20.balanceOf(nftSeller), 2, 'Incorrect ERC20 balance after trade')
-    assert.equal(await erc721.ownerOf(nfts[0]), nftBuyer, 'Incorrect ERC721 NFT owner after trade')
-  })
+        // NFT Seller
+        const selectorOne = web3.eth.abi.encodeFunctionSignature('ERC721ForERC20(bytes,address[7],uint8[2],uint256[6],bytes,bytes)')
+        const paramsOne = web3.eth.abi.encodeParameters(['address[2]', 'uint256[2]'], [[erc721.address, erc20.address], [nftTokenID, sellingPrice]]) 
+        const one = {registry: registry.address, maker: nftSeller, staticTarget: statici.address, staticSelector: selectorOne, staticExtradata: paramsOne, maximumFill: 1, listingTime: '0', expirationTime: '10000000000', salt: '11'}
+        const firstData = erc721c.methods.transferFrom(nftSeller, nftBuyer, nftTokenID).encodeABI()
+        const firstCall = {target: erc721.address, howToCall: 0, data: firstData}
+        let sigOne = await exchange.sign(one, nftSeller)
 
-//   it('purchase ERC721 NFT with ERC20 tokens',async () => {
-//     let {atomicizer, exchange, registry, statici, erc20, erc721} = await withContracts()
-//     let {tokens, nfts} = await withSomeTokens()
+        // NFT Buyer
+        const selectorTwo = web3.eth.abi.encodeFunctionSignature('ERC20ForERC721(bytes,address[7],uint8[2],uint256[6],bytes,bytes)')
+        const paramsTwo = web3.eth.abi.encodeParameters(['address[2]', 'uint256[2]'], [[erc20.address, erc721.address], [nftTokenID, buyingPrice]])
+        const two = {registry: registry.address, maker: nftBuyer, staticTarget: statici.address, staticSelector: selectorTwo, staticExtradata: paramsTwo, maximumFill: buyingPrice, listingTime: '0', expirationTime: '10000000000', salt: '12'}
+        const secondData = erc20c.methods.transferFrom(nftBuyer, nftSeller, buyingPrice).encodeABI()
+        const secondCall = {target: erc20.address, howToCall: 0, data: secondData}
+        let sigTwo = await exchange.sign(two, nftBuyer)
 
-//     await erc721.transferFrom(accounts[0], accounts[6], nfts[0], {from: accounts[0]})
-//     assert.equal(await erc20.balanceOf(accounts[6]), 0, 'Incorrect initial ERC20 balance')
-//     assert.equal(await erc721.ownerOf(nfts[0]), accounts[6], 'Incorrect initial ERC721 NFT owner')
+        // -------------- Execute the NFT Trade -------------- //
 
-//     const abi = [{'constant': false, 'inputs': [{'name': 'addrs', 'type': 'address[]'}, {'name': 'values', 'type': 'uint256[]'}, {'name': 'calldataLengths', 'type': 'uint256[]'}, {'name': 'calldatas', 'type': 'bytes'}], 'name': 'atomicize', 'outputs': [], 'payable': false, 'stateMutability': 'nonpayable', 'type': 'function'}]
-//     const atomicizerc = new web3.eth.Contract(abi, atomicizer.address)
-//     const erc20c = new web3.eth.Contract(erc20.abi, erc20.address)
-//     const erc721c = new web3.eth.Contract(erc721.abi, erc721.address)
-    
-//     const selectorOne = web3.eth.abi.encodeFunctionSignature('any(bytes,address[7],uint8[2],uint256[6],bytes,bytes)')
-//     const extradataOne = '0x'
-//     const one = {registry: registry.address, maker: accounts[0], staticTarget: statici.address, staticSelector: selectorOne, staticExtradata: extradataOne, maximumFill: '1', listingTime: '0', expirationTime: '10000000000', salt: '3352'}
+        await exchange.atomicMatchWith(one, sigOne, firstCall, two, sigTwo, secondCall, ZERO_BYTES32, {from: nftSeller})
 
-//     const selectorTwo = web3.eth.abi.encodeFunctionSignature('any(bytes,address[7],uint8[2],uint256[6],bytes,bytes)')
-//     const extradataTwo = '0x'
-//     const two = {registry: registry.address, maker: accounts[6], staticTarget: statici.address, staticSelector: selectorTwo, staticExtradata: extradataTwo, maximumFill: '1', listingTime: '0', expirationTime: '10000000000', salt: '3335'}
-        
-//     const firstERC20Call = erc20c.methods.transferFrom(accounts[0], accounts[6], 2).encodeABI()
-//     const firstData = atomicizerc.methods.atomicize(
-//       [erc20.address],
-//       [0],
-//       [(firstERC20Call.length - 2) / 2],
-//       firstERC20Call
-//     ).encodeABI()
-    
-//     const secondERC721Call = erc721c.methods.transferFrom(accounts[6], accounts[0], nfts[0]).encodeABI()
-//     const secondData = atomicizerc.methods.atomicize(
-//       [erc721.address],
-//       [0],
-//       [(secondERC721Call.length - 2) / 2],
-//       secondERC721Call
-//     ).encodeABI()
-    
-//     const firstCall = {target: atomicizer.address, howToCall: 1, data: firstData}
-//     const secondCall = {target: atomicizer.address, howToCall: 1, data: secondData}
-    
-//     const oneSig = NULL_SIG
+        // -------------- Verify the NFT Trade -------------- //
 
-//     let twoSig = await exchange.sign(two, accounts[6])
-//     await exchange.atomicMatch(one, oneSig, firstCall, two, twoSig, secondCall, ZERO_BYTES32)
-//     assert.equal(await erc20.balanceOf(accounts[6]), 2, 'Incorrect ERC20 balance after trade')
-//     assert.equal(await erc721.ownerOf(nfts[0]), accounts[0], 'Incorrect ERC721 NFT owner after trade')
-//   })
+        let nftSellerErc20Balance = await erc20.balanceOf(nftSeller)
+        let tokenOwner = await erc721.ownerOf(nftTokenID)
+        assert.equal(nftSellerErc20Balance.toNumber(), sellingPrice,'Incorrect ERC20 balance')
+        assert.equal(tokenOwner, nftBuyer,'Incorrect token owner')
+    })
 
 })
